43db5b73f8c60a8da41b17f854148e91a088ad2a
tcrone <tobin.cronus@gmail.com>
2017-03-30 00:36:29 -0400

    Two Room Library Examples.
    Two ways to use the RoomLibrary and RoomLibrary_HashMap.
    Give some feedback on what could be better to use. In the hashmap you're
    able to give a keyvalue to find any room object. parsing the arraylist
    used in RoomLibrary may be harder to do to find a specific room object.

--- Controller/GameController.java
+++ Controller/GameController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -21,29 +22,42 @@ public class GameController
 	 * 			Send appropriate object data to the View for display
 	 */
 	
-	
-	
 	public static void main(String[] args)
 	{
 		
-		//Instantiate
+		//Creating our Room Objects with the RoomLibrary_HashMap class
+		System.out.println("\nStarting RoomLibrary_HashMap example");
 		Map<String,Room> roomInfoMap = new HashMap<String,Room>();
 		RoomLibrary_HashMap roomlibrary = new RoomLibrary_HashMap();
 		
+		//Set 
 		roomInfoMap = roomlibrary.getRoomsAL();
 		
+		//Loop through and print every Key in the HashMap.
 		Iterator iterate = roomInfoMap.entrySet().iterator();
-		
-		/*while(iterate.hasNext())
+		while(iterate.hasNext())
 		{
 			Map.Entry roomobj = (Map.Entry<String, Room>)iterate.next();
 			System.out.println(roomobj.getKey().toString());
 			//System.out.println(roomlibrary.getRmDescript(roomobj.getKey().toString()));
-		}*/
+		}
 		
 		Room CurrentRoom = roomInfoMap.get("RM101");
 		
 		System.out.println(roomlibrary.getRmDescript(CurrentRoom.getRmId()));
+		
+		
+		//Trying with original RoomLibrary class
+		System.out.println("\nStarting RoomLibrary example");
+		RoomLibrary rl = new RoomLibrary();
+		ArrayList<Room> roomList = rl.roomsAL();
+		
+		for(int x = 0; x < roomList.size(); x++)
+		{			
+			Room thisroom = roomList.get(x);
+			System.out.println(thisroom.getRmId());
+		}
+		
 		
 	}
 
